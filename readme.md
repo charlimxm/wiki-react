5d9d38bb16644d4c482b4112d0ba3a64143c8fb5
Charmaine <charlimxm@gmail.com>
2018-03-07 14:20:57 +0800

    set up react, webpack and babel

---
+++ readme.md
@@ -0,0 +1,16 @@
+```
+.
+├── package.json
+├── dist
+│   └── index.html
+├── src
+│   └── app.js
+├── .babelrc
+└── webpack.config.js
+```
+
+**Steps**
+* Setting up project without create-react-app
+
+**Resources**
+* https://www.robinwieruch.de/minimal-react-webpack-babel-setup/
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// describes how webpack should transpile the code
+// uses the babel-loader for all files that has a .js or a .jsx extension
+
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/app.js'
+  ],
+  module: {
+  rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+}
+
+module.exports = config
