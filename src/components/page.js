acb2522a65b3c1a09fb7fda8eafaac95f1178dc4
Charmaine <charlimxm@gmail.com>
2018-03-13 15:56:04 +0800

    added wikipedia API to render titles of 10 random pages

--- src/components/page.js
+++ src/components/page.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react'
 
 class Page extends Component {
+  constructor (props) {
+    super(props)
 
-    render () {
-      return (
-        <div>
-            <h1>Page</h1>
-        </div>
-      )
+    this.state = {
+      pages: [],
+      requestFailed: false
     }
   }
+
+  componentDidMount () {
+    console.log('Before fetch')
+    fetch('https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&grnlimit=10&origin=*')
+      .then(console.log('Fetch successful'))
+      .then(response => {
+        if (!response.ok) {
+          throw Error('Error fetching API!')
+        }
+        return response
+      })
+      .then(data => data.json())
+      .then(data => {
+        let pageTitles = []
+        for (var i = 0; i < 10; i++) {
+          pageTitles.push(<li id={(Object.values(data.query.pages))[i].pageid}><strong>{(Object.values(data.query.pages))[i].title}</strong></li>)
+        }
+        this.setState({
+          pages: pageTitles
+        })
+        console.log(this.state.pages)
+      }, () => {
+        this.setState({
+          requestFailed: true
+        })
+      })
+  }
+
+  render () {
+    return (
+      <div className='randomPagesContainer'>
+        {this.state.pages}
+      </div>
+    )
+  }
+}
 
 export default Page
